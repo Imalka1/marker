e303e3c09f81db542679dbacc0c4323c71650107
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2018-12-29 19:09:12 +0530

    sql

--- maps.sql
+++ maps.sql
@@ -65,6 +65,13 @@ CREATE TABLE `markers` (
   `type` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+CREATE TABLE (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 --
 -- Dumping data for table `markers`
 --
